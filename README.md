77a73382e5d237e4890348aac0f70436ef1c2793
Dariusz Gwozdz <darekgw@interia.pl>
2020-03-08 12:25:57 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Web-components with LitElement library project starter
+
+tbd
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const env = process.env.NODE_ENV;
+
+module.exports = {
+    entry: './js/app.js',
+
+    mode: env,
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+        publicPath: '/'
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        compress: true,
+        port: 3500,
+        hot: true,
+    },
+
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.(sc|c)ss$/,
+                use: [
+                    env == 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
