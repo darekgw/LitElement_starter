2eb870ad20851b338d74bfcb25ab554fdb11979d
Dariusz Gwozdz <darekgw@interia.pl>
2020-03-12 20:14:58 +0100

    changed project structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,52 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const env = process.env.NODE_ENV;
 
 module.exports = {
-    entry: './js/app.js',
+  entry: "./js/app.js",
 
-    mode: env,
+  mode: env,
 
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-        publicPath: '/'
-    },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    publicPath: "/"
+  },
 
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        compress: true,
-        port: 3500,
-        hot: true,
-    },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    port: 3500,
+    hot: true
+  },
 
-    module: {
-        rules: [{
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.(sc|c)ss$/,
-                use: [
-                    env == 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader',
-                ]
-            }
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.(sc|c)ss$/,
+        use: [
+          env == "development" ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
         ]
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html'
-        }),
-        new webpack.HotModuleReplacementPlugin()
+      }
     ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin()
+  ]
 };
